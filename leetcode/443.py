a7a7dcba2bc32d8b4f45d4c651dd189b3eb20664
TowaYokoyama <191555241+TowaYokoyama@users.noreply.github.com>
2026-01-13 19:17:49 +0900

    D.py

---
+++ leetcode/443.py
@@ -0,0 +1,23 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        write = 0
+        read = 0
+        n = len(chars)
+
+        while read < n:
+            ch = chars[read]
+            count = 0
+
+            while read < n and chars[read] == ch:
+                read += 1
+                count += 1
+
+            chars[write] = ch
+            write += 1
+
+            if count > 1:
+                for c in str(count):
+                    chars[write] = c
+                    write += 1
+
+        return write
